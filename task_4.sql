9b2f6dee51abe2ce41d5f04263235ee0c0d59e32
Ernest Mimshach <mimshach.e@gmail.com>
2024-07-28 14:25:55 +0000

    made changes to file

--- task_4.sql
+++ task_4.sql
@@ -2,7 +2,12 @@ USE alx_book_store;
 USE alx_book_store;
 
 SELECT 
-   *
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
 FROM 
     INFORMATION_SCHEMA.COLUMNS
 WHERE 

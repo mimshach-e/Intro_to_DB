540f3ff1ae2e28444d6fc7fdbe2949f7ccb4f7c2
Ernest Mimshach <mimshach.e@gmail.com>
2024-07-28 01:56:26 +0000

    made changes to the sql file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 CREATE TABLE Books (
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
-    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id),
     price DOUBLE,
     publication_date DATE
 );
@@ -22,13 +22,13 @@ CREATE TABLE Customers (
 
 CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
-    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 );
 
 CREATE TABLE Order_Details (
     order_detail_id INT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 );

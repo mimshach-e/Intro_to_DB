52df8219d003736b7eb79ab0394eb3c5793cf9bb
Ernest Mimshach <mimshach.e@gmail.com>
2024-07-28 14:22:10 +0000

    added new sql file task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,10 @@
 USE alx_book_store;
-SHOW COLUMNS FROM books;
+USE alx_book_store;
+
+SELECT 
+   *
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_NAME = 'books' 
+    AND TABLE_SCHEMA = 'alx_book_store';
